1147514fcd60c4476a39b59132dac753d8a6b7d9
anandshakya6398 <prabhatshakya166@gmail.com>
2025-08-15 11:46:52 +0530

    add some new files

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -4,6 +4,25 @@ const Verification = require('../models/verificationModel');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const multer = require('multer');
+const nodemailer = require('nodemailer');
+const responseFunction = require('../utils/responseFunction');
+const fs = require('fs');
+const errorHandler = require('../middlewares/errorMiddleware');
+
+async function mailer(recieveremail, code){
+    let transporter = nodemailer.getTransporter({
+    host:"smtp.gmail.com",
+    port: process.env.EMAIL_PORT,
+    secure: false,
+    requireTLS: true,
+    auth:{
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+})
+}
+
 
 router.get('/test', (req, res) => {
     res.send('Test route is working');
