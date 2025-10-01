368ac80affd6536adc8d5706c693ebf95abf2357
anandshakya6398 <prabhatshakya166@gmail.com>
2025-10-01 11:07:00 +0530

    intial code

---
+++ Backend/utils/responseFunction.js
@@ -0,0 +1,8 @@
+const responseFunction = (res, status, message, data, ok) => { 
+    res.status(status).json({
+        message: message,
+        data: data,
+        ok: ok
+    });
+}
+module.exports = responseFunction;
