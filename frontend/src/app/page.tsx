f9d39fa75dfe2ba17b22eb2682293cf3af8dc5fa
anandshakya6398 <prabhatshakya166@gmail.com>
2025-10-07 10:26:27 +0530

    update

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,15 @@
 import Navbar from "@/components/Navbar";
+import { Share } from "next/font/google";
 import Image from "next/image";
+import FileSharePage from "./share/page";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
 <>
 <Navbar/>
+<FileSharePage/>
+<Footer/>
 </>
   );
 }
