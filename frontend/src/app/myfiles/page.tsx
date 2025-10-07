dc54c12652a1eeafac19bd73a09cc3f74e602e84
anandshakya6398 <prabhatshakya166@gmail.com>
2025-10-07 08:38:21 +0530

    initial code

--- frontend/src/app/myfiles/page.tsx
+++ frontend/src/app/myfiles/page.tsx
@@ -1,10 +1,46 @@
+import Navbar from '@/components/Navbar';
 import React from 'react'
 
 const page = () => {
+
+  const tempFiles = [
+  {
+    filename : "test1.pdf",
+    fileurl: '',
+    filetype: 'pdf',
+    sharedAt: new Date()
+  },
+  {
+    filename : "test2.mp4",
+    fileurl: '',
+    filetype: 'mp4',
+    sharedAt: new Date()
+  },
+  {
+    filename : "test3.png",
+    fileurl: '',
+    filetype: 'png',
+    sharedAt: new Date()
+  }
+
+  ];
   return (
+    <>
+    <Navbar/>
     <div>
-      
+      {
+        tempFiles.map((file, index) => {
+          return (
+            <div key={index} className="bg-white/10 backdrop-blur-md rounded-2xl shadow-xl p-2 mx-6 px-4  text-white my-4 ">
+              <h3 className="text-xl font-semibold mb-2">{file.filename}</h3>
+              <p className="text-sm text-gray-300 mb-4">Date & Time: {file.sharedAt.toLocaleString()}</p>
+            </div>
+          )
+        })
+        
+      }
     </div>
+        </>
   )
 }
 
