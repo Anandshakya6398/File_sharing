e9ecd367dac39cca0c2d8a4d266585a96c69738e
anandshakya6398 <prabhatshakya166@gmail.com>
2025-10-07 11:12:02 +0530

    update

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,13 +3,22 @@ import { Share } from "next/font/google";
 import Image from "next/image";
 import FileSharePage from "./share/page";
 import Footer from "@/components/Footer";
+import Why from "@/components/why";
+import How from "@/components/How";
+import Frequently from "@/components/Frequently";
+// import Why from "@/components/why";
+
 
 export default function Home() {
   return (
 <>
-<Navbar/>
-<FileSharePage/>
-<Footer/>
-</>
+      <Navbar />
+      <FileSharePage />
+      <Why />
+      <How/>
+      <Frequently/>
+      <Footer />
+    </>
   );
 }
+
